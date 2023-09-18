734ed0fff7a4015e8562de7fe5a3a955488b3744
Michael Hellman <michaelhellmaniii@gmail.com>
2023-09-18 11:00:42 -0500

    fix: fixed common dm failure

--- Olympia.py
+++ Olympia.py
@@ -33,7 +33,7 @@ def main():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials\credentials.json', SCOPES)
+                'credentials/credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
--- client.py
+++ client.py
@@ -42,16 +42,16 @@ class olympiaClient(discord.Client):
         self.roleUpdateLoop.start()
 
     
-    @tasks.loop(seconds=300)
+    @tasks.loop(minutes=10)
     async def roleUpdateLoop(self):
         await self.server.chunk()
         self.fetchSheetData()
-        self.mailMissingParticipants()
+        await self.mailMissingParticipants()
         await self.updateRoles()
         await self.messagePresents()
         
 
-    def mailMissingParticipants(self):
+    async def mailMissingParticipants(self):
         present = [m.name for m in self.server.members]
         missing = []
         for p in self.sheetData:
@@ -59,7 +59,7 @@ class olympiaClient(discord.Client):
                 missing.append(p)
         print(missing)
 
-        self.mailAbsentees(missing)
+        await self.mailAbsentees(missing)
         
                 
     def googleAuth(self):
@@ -72,7 +72,7 @@ class olympiaClient(discord.Client):
             
             #else:
         flow = InstalledAppFlow.from_client_secrets_file(
-            'credentials\credentials.json', SCOPES)
+            'credentials/credentials.json', SCOPES)
         credentials = flow.run_local_server(port=0)
             
             #with open('gToken.json', 'w') as token:
@@ -104,40 +104,41 @@ class olympiaClient(discord.Client):
     async def updateRoles(self):
         sheetNames = []
         for p in self.sheetData:
-            sheetNames.append(p[2])
+            sheetNames.append(p[2].lower())
         
         mailedTo = {}
         present = [m for m in self.server.members if m.name in sheetNames]
-        print(present)
-
+        names = [m.name for m in present]
+        print(names)
         with open("sentto.json", 'r') as sent:
             mailedTo = json.load(sent)
             for i, m in enumerate(present):
+                ind = sheetNames.index(m.name)
                 if m.name in mailedTo["discord"]:
                     continue
                 print("adding roles to " + m.name)
-                if self.sheetData[i][3] == "Red":
+                if self.sheetData[ind][3] == "Red":
                     print("adding red")
                     await m.add_roles(self.server.get_role(1024692661917065256))
                     await m.remove_roles(self.server.get_role(1024693348730155070), self.server.get_role(1024692666832781322), self.server.get_role(1024693270175039539))
-                if self.sheetData[i][3] == "Blue":
+                if self.sheetData[ind][3] == "Blue":
                     print("adding Blue")
                     await m.add_roles(self.server.get_role(1024693348730155070))
                     await m.remove_roles(self.server.get_role(1024692661917065256), self.server.get_role(1024692666832781322), self.server.get_role(1024693270175039539))
-                if self.sheetData[i][3] == "Yellow":
+                if self.sheetData[ind][3] == "Yellow":
                     print("adding yellow")
                     await m.add_roles(self.server.get_role(1024692666832781322))
                     await m.remove_roles(self.server.get_role(1024692661917065256), self.server.get_role(1024693348730155070), self.server.get_role(1024693270175039539))
-                if self.sheetData[i][3] == "Green":
+                if self.sheetData[ind][3] == "Green":
                     print("adding green")
                     await m.add_roles(self.server.get_role(1024693270175039539))
                     await m.remove_roles(self.server.get_role(1024692661917065256), self.server.get_role(1024693348730155070), self.server.get_role(1024692666832781322))
                 await m.add_roles(self.server.get_role(1088654298012987422))
-                await asyncio.sleep(2)
+                await asyncio.sleep(0.2)
                 print("added roles")
 
 
-    def mailAbsentees(self, absenteeList):
+    async def mailAbsentees(self, absenteeList):
         service = build('gmail', 'v1', credentials=self.gCreds)
         sent = open("sentto.json", 'r')
         mailedTo = json.load(sent)
@@ -162,64 +163,120 @@ class olympiaClient(discord.Client):
     async def messagePresents(self):
         sheetNames = [] #MESSAGING BLOCKED
         for p in self.sheetData:
-            sheetNames.append(p[2])
+            sheetNames.append(p[2].lower())
 
         mailedTo = {}
         present = [m for m in self.server.members if m.name in sheetNames]
-        print(present)
+        names = [m.name for m in present]
         with open("sentto.json", 'r') as sent:
             mailedTo = json.load(sent)
             for i, m in enumerate(present):
                 if m.name in mailedTo["discord"]:
                     continue
-                mailedTo["discord"].append(m.name)
+                ind = sheetNames.index(m.name)
                 if self.sheetData[i][3] != "No Preference":
-                    await m.send("Hello, and welcome to ACM Olympics! You have been placed on the " + self.sheetData[i][3] + ":" + self.sheetData[i][3].lower() + "_square: team! If this is your first Olympics, thanks for joining us this year, we're going to have a great time!")
-                    events = self.sheetData[i][4].split(", ")
-                    eventsString = ""
-                    for e in events:
-                        if ("Opening" in e):
-                            eventsString = eventsString + "Opening Ceremony :balloon: | 4:00 PM to 5:00 PM | Friday | ECSS 2.201\n\n"
-                            continue
-                        if ("Typing" in e):
-                            eventsString = eventsString + "Typing :keyboard: | 5:15 PM to 6:15 PM | Friday | ECSS 2.201\n\n"
-                            continue
-                        if ("Soccer" in e):
-                            eventsString = eventsString + "Soccer :soccer: | 6:30 PM to 8:30 PM | Friday | Soccer Field 8\n\n"
-                            continue
-                        if ("Push" in e):
-                            eventsString = eventsString + "Push Ups :muscle: | 11:00 PM to 11:30 AM | Saturday | Multipurpose Field\n\n"
-                            continue
-                        if ("One Mile" in e):
-                            eventsString = eventsString + "One Mile Run :athletic_shoe: | 11:30 AM to 12:00 PM | Saturday | Multipurpose Field\n\n"
-                            continue
-                        if ("Table" in e):
-                            eventsString = eventsString + "Table Tennis :Ping_Pong: | 12:15 PM to 1:45 PM | Saturday | SU 1st Floor\n\n"
-                            continue
-                        if ("Mystery" in e):
-                            eventsString = eventsString + "Mystery Event :question: | 2:00 PM to 3:00 PM | Saturday | ECSS 2.201\n\n"
-                            continue
-                        if ("Programming" in e):
-                            eventsString = eventsString + "Partner Programming :computer: | 3:15 PM to 4:15 PM | Saturday | ECSS 2.201\n\n"
-                            continue
-                        if ("Basketball" in e):
-                            eventsString = eventsString + "Basketball :basketball: | 4:30 PM to 6:30 PM | Saturday | Main Gym North Court\n\n"
-                            continue
-                        if ("Smash" in e):
-                            eventsString = eventsString + "Smash Bros :boxing_glove: | 6:45 PM to 8:45 PM | Saturday | ECSS 2.201\n\n"
-                            continue
-                        if ("Volleyball" in e):
-                            eventsString = eventsString + "Volleyball :volleyball: | 1:30 PM to 3:30 PM | Sunday | UTD Volleyball Courts\n\n"
-                            continue
-                        if ("Trivia" in e):
-                            eventsString = eventsString + "Trivia :brain: | 3:45 PM to 4:45 PM | Sunday | ECSS 2.201\n\n"
-                            continue
-                        if ("Closing" in e):
-                            eventsString = eventsString + "Closing Ceremony :pizza: | 5:30 PM to 6:30 PM | Sunday | ECSS 2.201\n\n"
-                            continue
-                    await m.send("As a quick reminder for your own reference, you signed up for the following events: \n\n" + eventsString)
-                    await m.send("We're sure you can't wait for the games to begin, so to tide yourself over, go and visit the #Olympics channel in the Discord, as well as your team channel, and get to know your fellow competitors! If you have any questions, message @Glassshatter on Discord. See you soon!")
-                    await asyncio.sleep(2)
+                    try:
+                        await m.send(" ")
+                        await m.send("Hello, and welcome to ACM Olympics! You have been placed on the " + self.sheetData[ind][3] + ":" + self.sheetData[ind][3].lower() + "_square: team! If this is your first Olympics, thanks for joining us this year, we're going to have a great time!")
+                        mailedTo["discord"].append(m.name)
+                        events = self.sheetData[ind][4].split(", ")
+                        eventsString = ""
+                        for e in events:
+                            if ("Opening" in e):
+                                eventsString = eventsString + "Opening Ceremony :balloon: | 4:00 PM to 5:00 PM | Friday | ECSS 2.201\n\n"
+                                continue
+                            if ("Typing" in e):
+                                eventsString = eventsString + "Typing :keyboard: | 5:15 PM to 6:15 PM | Friday | ECSS 2.201\n\n"
+                                continue
+                            if ("Soccer" in e):
+                                eventsString = eventsString + "Soccer :soccer: | 6:30 PM to 8:30 PM | Friday | Soccer Field 8\n\n"
+                                continue
+                            if ("Push" in e):
+                                eventsString = eventsString + "Push Ups :muscle: | 11:00 PM to 11:30 AM | Saturday | Multipurpose Field\n\n"
+                                continue
+                            if ("One Mile" in e):
+                                eventsString = eventsString + "One Mile Run :athletic_shoe: | 11:30 AM to 12:00 PM | Saturday | Multipurpose Field\n\n"
+                                continue
+                            if ("Table" in e):
+                                eventsString = eventsString + "Table Tennis :ping_pong: | 12:15 PM to 1:45 PM | Saturday | SU 1st Floor\n\n"
+                                continue
+                            if ("Mystery" in e):
+                                eventsString = eventsString + "Mystery Event :question: | 2:00 PM to 3:00 PM | Saturday | ECSS 2.201\n\n"
+                                continue
+                            if ("Programming" in e):
+                                eventsString = eventsString + "Partner Programming :computer: | 3:15 PM to 4:15 PM | Saturday | ECSS 2.201\n\n"
+                                continue
+                            if ("Basketball" in e):
+                                eventsString = eventsString + "Basketball :basketball: | 4:30 PM to 6:30 PM | Saturday | Main Gym North Court\n\n"
+                                continue
+                            if ("Smash" in e):
+                                eventsString = eventsString + "Smash Bros :boxing_glove: | 6:45 PM to 8:45 PM | Saturday | ECSS 2.201\n\n"
+                                continue
+                            if ("Volleyball" in e):
+                                eventsString = eventsString + "Volleyball :volleyball: | 1:30 PM to 3:30 PM | Sunday | UTD Volleyball Courts\n\n"
+                                continue
+                            if ("Trivia" in e):
+                                eventsString = eventsString + "Trivia :brain: | 3:45 PM to 4:45 PM | Sunday | ECSS 2.201\n\n"
+                                continue
+                            if ("Closing" in e):
+                                eventsString = eventsString + "Closing Ceremony :pizza: | 5:30 PM to 6:30 PM | Sunday | ECSS 2.201\n\n"
+                                continue
+                        await m.send("As a quick reminder for your own reference, you signed up for the following events: \n\n" + eventsString)
+                        await m.send("We're sure you can't wait for the games to begin, so to tide yourself over, go and visit the #Olympics channel in the Discord, as well as your team channel, and get to know your fellow competitors! If you have any questions, message @Glassshatter on Discord. See you soon!")
+                        print("Messaged")
+                        await asyncio.sleep(0.2)
+                    except:
+                        try:
+                            dmChannel = await m.create_dm()
+                            await dmChannel.send("Hello, and welcome to ACM Olympics! You have been placed on the " + self.sheetData[ind][3] + ":" + self.sheetData[ind][3].lower() + "_square: team! If this is your first Olympics, thanks for joining us this year, we're going to have a great time!")
+                            mailedTo["discord"].append(m.name)
+                            events = self.sheetData[ind][4].split(", ")
+                            eventsString = ""
+                            for e in events:
+                                if ("Opening" in e):
+                                    eventsString = eventsString + "Opening Ceremony :balloon: | 4:00 PM to 5:00 PM | Friday | ECSS 2.201\n\n"
+                                    continue
+                                if ("Typing" in e):
+                                    eventsString = eventsString + "Typing :keyboard: | 5:15 PM to 6:15 PM | Friday | ECSS 2.201\n\n"
+                                    continue
+                                if ("Soccer" in e):
+                                    eventsString = eventsString + "Soccer :soccer: | 6:30 PM to 8:30 PM | Friday | Soccer Field 8\n\n"
+                                    continue
+                                if ("Push" in e):
+                                    eventsString = eventsString + "Push Ups :muscle: | 11:00 PM to 11:30 AM | Saturday | Multipurpose Field\n\n"
+                                    continue
+                                if ("One Mile" in e):
+                                    eventsString = eventsString + "One Mile Run :athletic_shoe: | 11:30 AM to 12:00 PM | Saturday | Multipurpose Field\n\n"
+                                    continue
+                                if ("Table" in e):
+                                    eventsString = eventsString + "Table Tennis :ping_pong: | 12:15 PM to 1:45 PM | Saturday | SU 1st Floor\n\n"
+                                    continue
+                                if ("Mystery" in e):
+                                    eventsString = eventsString + "Mystery Event :question: | 2:00 PM to 3:00 PM | Saturday | ECSS 2.201\n\n"
+                                    continue
+                                if ("Programming" in e):
+                                    eventsString = eventsString + "Partner Programming :computer: | 3:15 PM to 4:15 PM | Saturday | ECSS 2.201\n\n"
+                                    continue
+                                if ("Basketball" in e):
+                                    eventsString = eventsString + "Basketball :basketball: | 4:30 PM to 6:30 PM | Saturday | Main Gym North Court\n\n"
+                                    continue
+                                if ("Smash" in e):
+                                    eventsString = eventsString + "Smash Bros :boxing_glove: | 6:45 PM to 8:45 PM | Saturday | ECSS 2.201\n\n"
+                                    continue
+                                if ("Volleyball" in e):
+                                    eventsString = eventsString + "Volleyball :volleyball: | 1:30 PM to 3:30 PM | Sunday | UTD Volleyball Courts\n\n"
+                                    continue
+                                if ("Trivia" in e):
+                                    eventsString = eventsString + "Trivia :brain: | 3:45 PM to 4:45 PM | Sunday | ECSS 2.201\n\n"
+                                    continue
+                                if ("Closing" in e):
+                                    eventsString = eventsString + "Closing Ceremony :pizza: | 5:30 PM to 6:30 PM | Sunday | ECSS 2.201\n\n"
+                                    continue
+                            await dmChannel.send("As a quick reminder for your own reference, you signed up for the following events: \n\n" + eventsString)
+                            await dmChannel.send("We're sure you can't wait for the games to begin, so to tide yourself over, go and visit the #Olympics channel in the Discord, as well as your team channel, and get to know your fellow competitors! If you have any questions, message @Glassshatter on Discord. See you soon!")
+                            print("Messaged with create DMChannel")
+                        except:
+                            print("double failure")
         with open("sentto.json", 'w') as f:
             json.dump(mailedTo, f)
             f.close()
